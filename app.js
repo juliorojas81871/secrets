10fdae5d1a4929e8a981c1269ab52a36812813fd
Julio <juliorojas81871@gmail.com>
2022-08-18 19:58:55 -0400

    heroku

--- app.js
+++ app.js
@@ -176,8 +176,8 @@ app.post("/logout", function(req, res){
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("app listening on port 5000")
+    console.log("app listening on port 3000")
 });
